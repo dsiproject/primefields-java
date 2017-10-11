488b56362412257fa9f8c45e87ca127acaa2cac0
Eric McCorkle <eric@metricspace.net>
2017-10-10 22:07:58 -0400

    Remove asserts, add copyright banners

--- src/main/java/net/metricspace/crypto/math/field/PrimeField.java
+++ src/main/java/net/metricspace/crypto/math/field/PrimeField.java
@@ -1,3 +1,34 @@
+/* Copyright (c) 2017, Eric McCorkle.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package net.metricspace.crypto.math.field;
 
 import java.io.IOException;
@@ -165,9 +196,6 @@ public abstract class PrimeField<Val extends PrimeField<Val>>
      */
     protected void set(final long[] digits,
                        final int idx) {
-        assert(idx < digits.length);
-        assert(this.digits.length <= digits.length - idx);
-
         for(int i = 0; i < this.digits.length; i++) {
             this.digits[i] = digits[i + idx];
         }
@@ -328,8 +356,6 @@ public abstract class PrimeField<Val extends PrimeField<Val>>
      * @see #digits
      */
     public boolean normalizedEquals(final Val b) {
-        assert(digits.length == b.digits.length);
-
         long out = 0;
 
         for(int i = 0; i < digits.length; i++) {
